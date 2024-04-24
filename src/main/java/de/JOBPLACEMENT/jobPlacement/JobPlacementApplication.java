50e45e1fa4ef244d9bd8eddcf9b6f3f124905ef9
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-04-24 20:57:20 +0200

    Default main, test & db configuration in appProp

---
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/JobPlacementApplication.java
@@ -0,0 +1,13 @@
+package de.JOBPLACEMENT.jobPlacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPlacementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/JOBPLACEMENT/jobPlacement/JobPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.JOBPLACEMENT.jobPlacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
