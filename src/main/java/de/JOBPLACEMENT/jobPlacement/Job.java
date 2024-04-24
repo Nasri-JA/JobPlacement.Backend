7d34476a7c87ac8067be045b0764d8a1bfe79f1c
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-04-24 20:54:28 +0200

    Add job model with attributes

---
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/Job.java
@@ -0,0 +1,79 @@
+package de.JOBPLACEMENT.jobPlacement;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String companyName;
+    private String location;
+    private String salary;
+    private String description;
+
+    public Job() {
+    }
+
+    public Job(String title, String companyName, String location, String salary, String description) {
+        this.title = title;
+        this.companyName = companyName;
+        this.location = location;
+        this.salary = salary;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
