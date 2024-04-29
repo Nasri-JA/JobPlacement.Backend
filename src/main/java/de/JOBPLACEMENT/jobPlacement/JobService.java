832d2ba74ff4995548aa6b6c097fd3dd078da670
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-04-29 13:29:05 +0200

    Define the mthodes for REST

--- src/main/java/de/JOBPLACEMENT/jobPlacement/JobService.java
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/JobService.java
@@ -3,6 +3,9 @@ package de.JOBPLACEMENT.jobPlacement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class JobService {
     private final JobRepository jobRepository;
@@ -22,6 +25,59 @@ public class JobService {
 
         return jobRepository.save(job);
     }
+
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+
+    public Job updateJob(Long id, Job job) {
+        Optional<Job> optionalJob = jobRepository.findById(id);
+        if (optionalJob.isPresent()) {
+            Job existingJob = optionalJob.get();
+            existingJob.setTitle(job.getTitle());
+            existingJob.setCompanyName(job.getCompanyName());
+            existingJob.setLocation(job.getLocation());
+            existingJob.setSalary(job.getSalary());
+            existingJob.setDescription(job.getDescription());
+            return jobRepository.save(existingJob);
+        } else {
+            throw new NotFoundException("Job mit der ID: " + id + " nicht gefunden!");
+        }
+    }
+
+    public Job partialUpdateJob(Long id, Job job) {
+        Optional<Job> optionalJob = jobRepository.findById(id);
+        if (optionalJob.isPresent()) {
+            Job existingJob = optionalJob.get();
+            if (job.getTitle() != null) {
+                existingJob.setTitle(job.getTitle());
+            }
+            if (job.getCompanyName() != null) {
+                existingJob.setCompanyName(job.getCompanyName());
+            }
+            if (job.getLocation() != null) {
+                existingJob.setLocation(job.getLocation());
+            }
+            if (job.getSalary() != null) {
+                existingJob.setSalary(job.getSalary());
+            }
+            if (job.getDescription() != null) {
+                existingJob.setDescription(job.getDescription());
+            }
+            return jobRepository.save(existingJob);
+        } else {
+            throw new NotFoundException("Job mit der ID: " + id + " nicht gefunden!");
+        }
+    }
+
+    public void deleteJob(Long id) {
+        if (jobRepository.existsById(id)) {
+            jobRepository.deleteById(id);
+        } else {
+            throw new NotFoundException("Job mit der ID: " + id + " nicht gefunden!");
+        }
+    }
+
 }
 
 
