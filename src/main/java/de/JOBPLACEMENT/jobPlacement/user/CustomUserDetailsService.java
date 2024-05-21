0ecbc7223007389b4130e949bc7eb9d5d8788981
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-05-21 14:28:12 +0200

    Add Services for user

---
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/user/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package de.JOBPLACEMENT.jobPlacement.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Benuzter nicht gefunden"));
+
+        org.springframework.security.core.userdetails.User.UserBuilder builder =
+                org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.roles(user.getRoles().toArray(new String[0]));
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/user/UserService.java
@@ -0,0 +1,21 @@
+package de.JOBPLACEMENT.jobPlacement.user;
+
+import de.JOBPLACEMENT.jobPlacement.user.User;
+import de.JOBPLACEMENT.jobPlacement.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
