319c30dc483a8ec3baa44c62c005de395f1635fd
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-05-21 14:27:20 +0200

    Upade User model

--- src/main/java/de/JOBPLACEMENT/jobPlacement/user/User.java
+++ src/main/java/de/JOBPLACEMENT/jobPlacement/user/User.java
@@ -1,6 +1,8 @@
 package de.JOBPLACEMENT.jobPlacement.user;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
 
 
@@ -11,6 +13,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email
+    @NotBlank
     @Column(nullable = false, unique = true)
     private String username;
 
@@ -21,4 +25,37 @@ public class User {
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
     private Set<String> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
 }
